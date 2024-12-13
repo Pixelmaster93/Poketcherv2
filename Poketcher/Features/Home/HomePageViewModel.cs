7fa9eb9251222c2c64d9a9ee82c6862d02636877
Dario Tarasco <dario.tarasco@remira.com>
2024-12-13 11:45:00 +0100

    binding homepagepokemon

--- Poketcher/Features/Home/HomePageViewModel.cs
+++ Poketcher/Features/Home/HomePageViewModel.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
 using Poketcher.Features.Base;
+using Poketcher.Features.Pokemon.Home;
 using Poketcher.navigation.Navigation.Int;
 using System;
 using System.Collections.Generic;
@@ -13,6 +15,12 @@ namespace Poketcher.Features.Home
     {
         public HomePageViewModel(INavigationService navigationService, ILogger<BaseViewModel> logger, IAlertService alertService) : base(navigationService, logger, alertService)
         {
+        }
+
+        [RelayCommand]
+        public async Task GoToPokemonHome()
+        {
+            await Push<PokemonHomePage>();
         }
     }
 }
