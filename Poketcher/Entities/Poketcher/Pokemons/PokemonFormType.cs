1f254b33226946e896634bba957a7d39234cf5c4
Pixelmaster993 <dinoresin93@gmail.com>
2025-03-25 14:24:56 +0100

    add some Entities

---
+++ Poketcher/Entities/Poketcher/Pokemons/PokemonFormType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Poketcher.Entities.Poketcher.Pokemons
+{
+    public class PokemonFormType
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Slot { get; set; }
+
+        public int PokemonFormId { get; set; }
+        public PokemonForm PokemonForm { get; set; }
+
+        public int TypeId { get; set; }
+        public Type Type { get; set; }
+    }
+}
---
+++ Poketcher/Entities/Poketcher/Pokemons/PokemonHabitatName.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Poketcher.Entities.Poketcher.Languages;
+
+namespace Poketcher.Entities.Poketcher.Pokemons
+{
+    public class PokemonHabitatName
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int LanguageId { get; set; }
+        public Language Language { get; set; }
+
+        [ForeignKey("PokemonHabitat")]
+        public int HabitatId { get; set; }
+        public PokemonHabitat PokemonHabitat { get; set; }
+    }
+}
+
